20b4c02ac694e4b4ac9f9968e885857ae48d78b4
AltruisticGamer <marcello.marrufo@gmail.com>
2017-11-10 10:18:32 +1100

    adding project

---
+++ Assets/2-Functions/Scripts/Movement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Movement : MonoBehaviour
+{
+    public float movementSpeed = 20f;
+    public float rotationSpeed = 20f;
+
+    // Use this for input and Updates
+    void Update()
+    {
+        // check if user presses W
+        if (Input.GetKey(KeyCode.W))
+        {
+            // Move up
+            transform.Translate(new Vector3(0, movementSpeed, 0) * Time.deltaTime);
+        }
+        // check if user presses S
+        if (Input.GetKey(KeyCode.S))
+        {
+            // Move down
+            transform.Translate(new Vector3(0, -movementSpeed, 0) * Time.deltaTime);
+        }
+        // check if user presses D
+        if (Input.GetKey(KeyCode.D))
+        {
+            // Move Right
+            transform.Translate(new Vector3(movementSpeed, 0, 0) * Time.deltaTime);
+        }
+        // check if user presses A
+        if (Input.GetKey(KeyCode.A))
+        {
+            // Move Right
+            transform.Translate(new Vector3(-movementSpeed, 0, 0) * Time.deltaTime);
+        }
+    }
+
+    // Play at Start
+    void Start()
+    {
+        print("PROGRAMMING IS FUN");
+
+    }
+}
---
+++ Assets/2-Functions/Scripts/Rigid2dMovement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Functions
+{
+
+    public class Rigid2dMovement : MonoBehaviour
+    {
+        public float dashSpeed = 50f;
+        public float rotationSpeed = 5f;
+        public float acceleration = 20f;
+        public float deceleration = 0.1f;
+
+        private Rigidbody2D rigid2D;
+
+        // Use this for initialization
+        void Start()
+        {
+            // Get rigidbody component
+            rigid2D = GetComponent<Rigidbody2D>();
+        }
+        // Above Update
+
+        // Update is called once per frame
+        void Update()
+        {
+            //If we press the spacebar
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                // Make the player Dash
+                Dash();
+            }
+            // Calling a Function
+            Accelerate();
+            Decelerate();
+            Rotate();
+        }
+        // Under Update
+        void Accelerate()
+        {
+            // Get Input from vertical axes
+            float inputV = Input.GetAxis("Vertical");
+            // Add force to the right direction of the Player
+            rigid2D.AddForce(transform.up * inputV * acceleration);
+        }
+        void Decelerate()
+        {
+            // velocity += velocity * deceleration
+            rigid2D.velocity += -rigid2D.velocity * deceleration;
+        }
+        void Dash()
+        {
+            rigid2D.AddForce(transform.up * dashSpeed, ForceMode2D.Impulse);        
+        }
+        void Rotate()
+        {
+            // Get Horizontal Input
+            float inputH = Input.GetAxis("Horizontal");
+            // Perform rotation Input
+            transform.Rotate(Vector3.forward, rotationSpeed * -inputH);
+        }
+
+    }
+}
